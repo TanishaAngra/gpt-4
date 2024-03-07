0ce2af5f1c01eb44d7b03a7599031c08c45bcde0
TanishaAngra <tanishaangra1511@gmail.com>
2024-03-07 17:49:51 +0530

    cta chnage

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => (
   <div className="gpt4__cta">
     <div className="gpt4__cta-content">
       <p>Request Early Access to Get Started</p>
-      <h3>Register Today & start exploring the endless possibilities.</h3>
+      <h3>Register Today and start exploring the endless possibilities.</h3>
     </div>
     <div className="gpt4__cta-btn">
       <button type="button">Get Started</button>
